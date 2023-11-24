ad7a45da7079a315a458c7022f35017edd511266
aniketkumar <kaniket123456789@gmail.com>
2023-11-24 22:53:22 +0530

    understanding local, instance , static variables

--- scope.java
+++ scope.java
@@ -1,23 +1,33 @@
-class A{
-    int a=10;
-    static int b=56;
-    void add(){
-        int c=0;
-        c=a+b;
+class A {
+    int a;
+    static int b;
+
+    void add() {
+        int c;
+        c = a + b;
         System.out.println(c);
-        a=90;
     }
-    void mul(){
-        int d=1;
-        d=a*b;
+
+    void mul() {
+        int d ;
+        d = a * b;
         System.out.println(d);
-        b=66;
     }
-    public static void main(String args[]){
-        A obj=new A();
+
+    public static void main(String args[]) {
+        A obj = new A();
         obj.add();
         obj.mul();
+        // Instance variables can be called only with object creation.
+        obj.a = 1000;
+        b = 2000;
         System.out.println(obj.a);
         System.out.println(b);
+        A obj1=new A();
+        // when object is created memory is allocated for "a" in heap as its a instance variable.
+        System.out.println(obj1.a);
+        // we get the earlier default that was intialized during the start of the program.
+        System.out.println(obj1.b);
+        // changes show as varible is allocated memeory in static memory block, can be called with or without object.
     }
 }
