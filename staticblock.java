ce8cddbc243a30c7a88912c4cecb27c2a91eba5e
aniketkumar <kaniket123456789@gmail.com>
2023-11-30 15:05:11 +0530

    used static block

---
+++ staticblock.java
@@ -0,0 +1,12 @@
+class Staticblock{
+    
+
+    public static void main(String args[]){
+        System.out.println("main block");
+    }
+    
+    static{
+        System.out.println("static block");
+    }
+    
+}
