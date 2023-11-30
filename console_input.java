bd29f51e8a91ea70259721f8bf2d44472119a71c
aniketkumar <kaniket123456789@gmail.com>
2023-11-30 10:50:28 +0530

    added a console input loop

--- console_input.java
+++ console_input.java
@@ -1,7 +1,9 @@
 class console {
     public static void main(String args[]) {
 
-        System.out.println("Printing " + args[0] + "\t" + args[1]);
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
 
     }
 }
