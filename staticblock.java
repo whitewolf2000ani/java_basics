060673c06600ba3a8a2e1dc398fcf60caabf6ddd
aniketkumar <kaniket123456789@gmail.com>
2023-11-30 17:00:30 +0530

    small changes

--- staticblock.java
+++ staticblock.java
@@ -1,10 +1,8 @@
 class Staticblock{
-    
-
     public static void main(String args[]){
         System.out.println("main block");
     }
-    
+
     static{
         System.out.println("static block");
     }
