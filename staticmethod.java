d1561238891546aa61ad1d747a463d202f609d99
aniketkumar <kaniket123456789@gmail.com>
2023-11-30 16:59:31 +0530

    minor changes

--- staticmethod.java
+++ staticmethod.java
@@ -1,21 +1,22 @@
-class UsingStaticMethod{
+class UsingStaticMethod {
     int age;
-    static String name="IITM";
+    static String name = "IITM";
 
-    static void change(){
-        name="TINT";
+    static void change() {
+        name = "TINT";
     }
 
-    void display(){
-        System.out.println("Name:"+name+"\t"+"Age:"+age);
+    void display() {
+        System.out.println("Name:" + name + "\t" + "Age:" + age);
 
     }
 
 }
-class USM{
-    public static void main(String args[]){
-        UsingStaticMethod obj=new UsingStaticMethod();
-        obj.age=19;
+
+class USM {
+    public static void main(String args[]) {
+        UsingStaticMethod obj = new UsingStaticMethod();
+        obj.age = 19;
         obj.display();
         UsingStaticMethod.change();
         obj.display();
